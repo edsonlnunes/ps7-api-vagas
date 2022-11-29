45536daef421b435f582be670ccaf2a0d3a6fb5b
Edson Martins <edsonmartins3110@gmail.com>
2022-11-28 22:37:23 -0300

    feat: adiciona rotas da feature users no server

--- src/main/config/routes.ts
+++ src/main/config/routes.ts
@@ -1,5 +1,9 @@
 import { Express } from "express";
+import usersRoutes from "../../app/features/users/users.routes";
 
 export default (app: Express) => {
   app.get("/", (request, response) => response.status(200).send("API RODANDO"));
+
+  // rotas da feature users
+  app.use(usersRoutes())
 };

64d8c8ab09f18c54487ccb1fa0ba3e9e212c3f01
rafael-correa98 <rafaelcorrean98@gmail.com>
2022-11-29 21:49:12 -0300

    feat: controller login

--- src/app/features/users/controllers/user.controller.ts
+++ src/app/features/users/controllers/user.controller.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from "express";
 import CreateUser from "../usecases/create-user.usecase";
+import LoginUser from "../usecases/login-user.usercase";
 
 export default class UserController {
   async createUser(request: Request, response: Response) {
@@ -9,6 +10,7 @@ export default class UserController {
       const result = await usecase.execute(request.body);
 
       return response.status(200).json(result);
+
     } catch (error: any) {
       return response.status(500).json({ error: error.message, stack: error });
     }
@@ -19,5 +21,22 @@ export default class UserController {
     // mandar executar usecase
     // retornar o que usecase devolver
     // tratativa de erro
+  }
+
+  async loginUser(request: Request, response: Response) {
+    try {
+      console.log(request.headers.authorization);
+
+      const { username, password } = request.body;
+      // const username = request.body.username;
+      // const password = request.body.password;
+      const useCase = new LoginUser();
+
+      const token = await useCase.execute({ username, password })
+
+      return response.status(200).json(token)
+    } catch (error: any) {
+      return response.status(400).json({ error: error.message, stack: error })
+    }
   }
 }

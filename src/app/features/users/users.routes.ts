923a9c97f35a7c21ea004c85d53411690e81f713
rafael-correa98 <rafaelcorrean98@gmail.com>
2022-11-29 21:48:51 -0300

    feat: adicionei rota de login

--- src/app/features/users/users.routes.ts
+++ src/app/features/users/users.routes.ts
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import UserController from "./controllers/user.controller";
 import CreateUserValidator from "./validators/create-user.validator";
+import LoginUserValidator from "./validators/login-user.validator";
 
 export default () => {
   const router = Router();
@@ -12,6 +13,12 @@ export default () => {
     new CreateUserValidator().validate,
     userController.createUser
   );
+
+  router.post(
+    "/users/login",
+    new LoginUserValidator().validate,
+    userController.loginUser
+  )
 
   return router;
 };

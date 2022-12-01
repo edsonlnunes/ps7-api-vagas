40f469e1eaf23388648552f69c12282491d91fb3
Edson Martins <edsonmartins3110@gmail.com>
2022-11-30 21:16:34 -0300

    feat: adiciona rotas para vagas e add prefixo para as rotas de users

--- src/main/config/routes.ts
+++ src/main/config/routes.ts
@@ -1,11 +1,13 @@
 import { Express } from "express";
 import authRoutes from "../../app/features/authentication/auth.routes";
+import jobsRoutes from "../../app/features/jobs/jobs.routes";
 import usersRoutes from "../../app/features/users/users.routes";
 
 export default (app: Express) => {
   app.get("/", (request, response) => response.status(200).send("API RODANDO"));
 
   // rotas da feature users
-  app.use(usersRoutes());
+  app.use("/users", usersRoutes());
   app.use(authRoutes());
+  app.use("/jobs", jobsRoutes());
 };

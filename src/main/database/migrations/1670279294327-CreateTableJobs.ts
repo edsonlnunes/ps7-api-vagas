d13ba7fa815fde89f3a59c1af8d7dc04fdffed14
Edson Martins <edsonmartins3110@gmail.com>
2022-12-05 19:44:11 -0300

    feat: cria tabelas para vagas e relacionamento entre candidatos e vagas

---
+++ src/main/database/migrations/1670279294327-CreateTableJobs.ts
@@ -0,0 +1,47 @@
+import {
+  MigrationInterface,
+  QueryRunner,
+  Table,
+  TableForeignKey,
+} from "typeorm";
+
+export class CreateTableJobs1670279294327 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: "jobs",
+        columns: [
+          { name: "id", type: "uuid", isNullable: false, isPrimary: true },
+          { name: "description", type: "text", isNullable: false },
+          { name: "company", type: "varchar", length: "50", isNullable: false },
+          { name: "limit_date", type: "date", isNullable: false },
+          { name: "status", type: "boolean", isNullable: false },
+          { name: "max_applicant", type: "int", isNullable: true },
+          { name: "id_recruiter", type: "uuid", isNullable: false },
+          {
+            name: "updated_at",
+            type: "timestamp",
+            isNullable: true,
+          },
+          {
+            name: "created_at",
+            type: "timestamp",
+            isNullable: true,
+          },
+        ],
+        foreignKeys: [
+          new TableForeignKey({
+            name: "fk_jobs_users",
+            columnNames: ["id_recruiter"],
+            referencedTableName: "users",
+            referencedColumnNames: ["id"],
+          }),
+        ],
+      })
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable("jobs", true, true, true);
+  }
+}
---
+++ src/main/database/migrations/1670279795195-CreateTableCandidatesJobs.ts
@@ -0,0 +1,44 @@
+import {
+  MigrationInterface,
+  QueryRunner,
+  Table,
+  TableForeignKey,
+} from "typeorm";
+
+export class CreateTableCandidatesJobs1670279795195
+  implements MigrationInterface
+{
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: "candidates_jobs",
+        columns: [
+          { name: "id", type: "uuid", isNullable: false, isPrimary: true },
+          { name: "job_id", type: "uuid", isNullable: false },
+          { name: "candidate_id", type: "uuid", isNullable: false },
+          { name: "success_indicator", type: "boolean", isNullable: true },
+          { name: "updated_at", type: "timestamp", isNullable: true },
+          { name: "created_at", type: "timestamp", isNullable: true },
+        ],
+        foreignKeys: [
+          new TableForeignKey({
+            name: "fk_candidates_jobs_jobs",
+            columnNames: ["job_id"],
+            referencedTableName: "jobs",
+            referencedColumnNames: ["id"],
+          }),
+          new TableForeignKey({
+            name: "fk_candidates_jobs_users",
+            columnNames: ["candidate_id"],
+            referencedTableName: "users",
+            referencedColumnNames: ["id"],
+          }),
+        ],
+      })
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable("candidates_jobs", true, true, true);
+  }
+}

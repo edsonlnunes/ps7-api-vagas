96928f593d9e69a66884342f5366281f31d344ed
Edson Martins <edsonmartins3110@gmail.com>
2022-12-05 20:36:21 -0300

    feat: implementa metodo no repositorio para salvar uma vaga

--- src/app/features/jobs/repositories/job.repository.ts
+++ src/app/features/jobs/repositories/job.repository.ts
@@ -1,7 +1,21 @@
 import Job from "../../../models/job";
+import dataSource from "../../../../main/database/database-connection";
+import { JobEntity } from "../../../shared/database/entities/job.entity";
 
 export default class JobRepository {
   async createJob(job: Job): Promise<void> {
-    //
+    const manager = dataSource.manager;
+
+    const jobEntity = manager.create(JobEntity, {
+      id: job.id,
+      description: job.description,
+      company: job.company,
+      limitDate: job.deadline,
+      maxApplicant: job.maxCandidates,
+      recruiterId: job.recruiter.id,
+      status: job.enable,
+    });
+
+    await manager.save(jobEntity);
   }
 }

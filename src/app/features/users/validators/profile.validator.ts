ddc1c5ef9795248f2a10f76cba2317214b4bd764
Edson Martins <edsonmartins3110@gmail.com>
2022-12-05 19:19:13 -0300

    lint: ajusta identacao

--- src/app/features/users/validators/profile.validator.ts
+++ src/app/features/users/validators/profile.validator.ts
@@ -1,17 +1,16 @@
 import { NextFunction, Request, Response } from "express";
-import { EProfile, ExpProfile } from '../../../shared/enums/profile.enum'
+import { EProfile, ExpProfile } from "../../../shared/enums/profile.enum";
 
 export class ProfileValidator {
-
   validate(request: Request, response: Response, next: NextFunction) {
-    const profile = request.userAuth.profile.toUpperCase() as ExpProfile; 
+    const profile = request.userAuth.profile.toUpperCase() as ExpProfile;
 
-    if(EProfile.ADMIN !== profile) {
+    if (EProfile.ADMIN !== profile) {
       return response.status(403).json({
-         error: "Operação somente válida para usuários administradores" 
+        error: "Operação somente válida para usuários administradores",
       });
     }
-   
+
     next();
   }
 }

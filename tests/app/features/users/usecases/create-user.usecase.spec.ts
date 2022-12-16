492db0226e9f13d513c12e5554ef2572972a88b9
rafael-correa98 <rafaelcorrean98@gmail.com>
2022-12-15 21:52:45 -0300

    test: Teste de criaçao de usuario com sucesso

--- tests/app/features/users/usecases/create-user.usecase.spec.ts
+++ tests/app/features/users/usecases/create-user.usecase.spec.ts
@@ -20,7 +20,7 @@ describe("Testar a criação do usuário", () => {
     ).rejects.toThrowError("Já existe um usuário com este username");
   });
 
-  test.skip("Teste de erro com try catch", async () => {
+  test("Teste de erro com try catch", async () => {
     const repository = new UserRepository();
     const sut = new CreateUser(repository);
     jest
@@ -41,4 +41,33 @@ describe("Testar a criação do usuário", () => {
       
     }
   });
+
+  test("Teste de criaçao de usuario com sucesso", async () => {
+    const repository = new UserRepository();
+    const sut = new CreateUser(repository);
+
+    jest
+      .spyOn(UserRepository.prototype, "verifyUserExistsByUsername")
+      .mockResolvedValue(false);
+
+    jest
+      .spyOn(UserRepository.prototype, "createUser")
+      .mockResolvedValue();
+
+      const response = await sut.execute({
+        name: "any_name",
+        username: "any_username",
+        profile: "CANDIDATE",
+        company: "any_cia",
+        password: "123",
+      })
+
+      expect.assertions(6)
+      expect(response.id).toBeTruthy()
+      expect(response.name).toBe("any_name")
+      expect(response.username).toBe("any_username")
+      expect(response.profile).toBe("CANDIDATE")
+      expect(response.company).toBeUndefined()
+      expect(response.password).toBeUndefined()
+  })
 });

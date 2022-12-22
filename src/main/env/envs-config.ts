18e50a63d6d63f6f698c22dca75927b00cb79f35
rafael-correa98 <rafaelcorrean98@gmail.com>
2022-12-22 20:04:51 -0300

    feat: configuraçao Redis

--- src/main/env/envs-config.ts
+++ src/main/env/envs-config.ts
@@ -4,6 +4,7 @@ const envsConfig = {
   DATABASE_URL: process.env.DATABASE_URL,
   SECRET_TOKEN: process.env.SECRET_TOKEN,
   EXPIRE_IN: process.env.EXPIRE_IN,
+  REDIS_URL: process.env.REDIS_URL,
 };
 
 export default envsConfig;
